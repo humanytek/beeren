8b6565da4c44bf711ace861f72ee221ec77a44a6
Fernando Rodriguez <f.rodriguez@beeren.com.mx>
2023-02-03 19:19:56 -0600

    Factura
    1.9

--- factura_formulario/__manifest__.py
+++ factura_formulario/__manifest__.py
@@ -1,7 +1,15 @@
 {
-  "name": "Factura Beeren",
-  "depends": ["account"],
-  "data": [
-    "views/factura_beeren.xml",
-  ],
+    'name': 'Factura Beeren',
+    'version': '15.0.0.1.0',
+    'author': 'Fernando Rodriguez',
+    'summary': 'Module to hide Account field in Invoice Lines',
+    'category': 'Accounting',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/factura_beeren.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
 }
