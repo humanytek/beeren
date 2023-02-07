44960c1cf03895f384162a57c52357a611a2524a
Fernando Rodriguez <f.rodriguez@beeren.com.mx>
2023-02-07 12:22:23 -0600

    FACTURA
    1

--- factura_formulario/models/factura_beeren.py
+++ factura_formulario/models/factura_beeren.py
@@ -1,10 +1,3 @@
 from odoo import fields, models
 
 
-class factura_beeren (models.Model):
-    _inherit = "product.template"
-
-    Asin = fields.Char(
-        related ='product_id.Asin',
-        string= 'Asin',
-    )
