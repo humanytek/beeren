87beb12802701b851d2894c9a3cf05e760ef8652
ferguez1989 <f.rodriguez@beeren.com.mx>
2024-03-13 11:55:53 -0600

    Inventario 2

--- inventario_gser_2/models/inventario_gser_2.py
+++ inventario_gser_2/models/inventario_gser_2.py
@@ -2,8 +2,11 @@ from odoo import fields, models
 
 
 class inventario_gser_2 (models.Model):
-    _inherit = "stock.move.line"
+    _inherit = "stock.picking"
 
-    folio_1 = fields.Char(
-        string= 'Folio',
+    x_studio_folio_opd = fields.Char(
+        string= 'Folio OPD',
+    )
+    x_studio_folio_o = fields.Char(
+        string= 'Folio O',
     )
